64cad00cd796d5d82e6fe8d6cc899bcdda8356cb
Yehansa <insarid7@gmail.com>
2026-02-19 01:54:38 +0000

    maybe completed mbot driver file

--- src/mbot_driver/mbot_driver.cpp
+++ src/mbot_driver/mbot_driver.cpp
@@ -9,7 +9,105 @@ MBotDriver::MBotDriver(std::unique_ptr<interfaces::Server> server, std::unique_p
 }
 
 /**< TODO */
-void MBotDriver::spin(std::unique_ptr<interfaces::Notification> notif) {}
+void MBotDriver::spin(std::unique_ptr<interfaces::Notification> notif) {
+    //run until notifincation is triggered
+
+    while (!notif->is_ready()) {
+        std::weak_ptr<interfaces::Connection> new_connection;
+
+        //accept a cleint connection
+        if (!server->accept(new_connection)) {
+            continue;
+        }
+
+        {
+            std::lock_guard<std::mutex> lock(connection_mtx);
+            connection = new_connection;
+
+        }
+
+        auto shared_conn = new_connection.lock();
+        if (!shared_conn) continue;
+
+        Endpoint remote = shared_conn->remote_endpoint();
+
+        //connect clinet to same addres but port 8300
+        Endpoint goal_server(remote.address, 8300);
+
+        if (!client->connect(goal_server)) {
+            client->reset();
+            continue;
+        }
+
+        //read goal pose2dstamped msgs
+        while (!notif->is_ready()) {
+            //first read 4 byte size 
+
+            UInt32 size_msg;
+            ssize_t n = client->read(reinterpret_cast<uint8_t*>(&size_msg), sizeof(UInt32));
+
+            if (n <= 0) {
+                client->reset();
+                break;
+            }
+
+            uint32_t msg_size = size_msg.data;
+
+            std::vector<uint8_t> buffer(msg_size);
+
+            ssize_t total_read = 0;
+            while (total_read < (ssize_t)msg_size) {
+                ssize_t r = client->read(buffer.data() + total_read, msg_size - total_read);
+
+                if (r <= 0) {
+                    client->reset();
+                    break;
+                }
+
+                total_read += r;
+
+            }
+
+            if (total_read != (ssize_t)msg_size) {
+                break;
+            }
+
+            Pose2DStamped goal_pose;
+            size_t bytes_read = 0;
+            goal_pose.deserialize(buffer.data(), buffer.size(), bytes_read);
+
+            mbot->drive_to(goal_pose);
+
+        }
+
+    }
+}
 
 /**< TODO */
-void MBotDriver::on_pose_callback(const Pose2DStamped &pose) {}
+void MBotDriver::on_pose_callback(const Pose2DStamped &pose) {
+    std::lock_guard<std::mutex> lock(connection_mtx);
+
+    auto shared_conn = connection.lock();
+    if (!shared_conn) return;
+
+    size_t msg_size = pose.size();
+
+    //allocate buffer
+    std::vector<uint8_t> buffer(msg_size);
+
+    //serialize into buffer
+    size_t offset = 0;
+    pose.serialize(buffer.data(), offset);
+
+    //prefix with 4 byts size
+    UInt32 size_prefix;
+    size_prefix.data = static_cast<uint32_t>(msg_size);
+
+    //send size prefix
+    if (shared_conn->write(reinterpret_cast<uint8_t*>(&size_prefix), sizeof(UInt32)) <= 0) {
+        return;
+    }
+
+    shared_conn->write(buffer.data(), msg_size);
+
+}
